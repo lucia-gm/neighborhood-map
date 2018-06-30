4c4ea3219cccac3ba69d14e4de6580ed40b61f5c
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-06-30 12:42:56 -0700

    refactor: Defaultplaces to venuesApi

--- src/App.js
+++ src/App.js
@@ -8,28 +8,15 @@ class App extends Component {
   constructor(props) {
     super()
     this.state = {
-      locations: this.pointsOfInterest,
+      locations: [],
       filteredLocations: []
     }
   }
 
-  pointsOfInterest = [
-    {id: 0, name:'Praza do Obradoiro', location: {lat: 42.880593, lng: -8.545628}},
-    {id: 1, name: 'Mercado de Abastos', location: {lat: 42.879840, lng: -8.541342}},
-    {id: 2, name: 'Parque da Alameda', location: {lat: 42.876943, lng: -8.547609}},
-    {id: 3, name: 'A Maceta', location: {lat: 42.881636, lng: -8.535520}},
-    {id: 4, name: 'Casa Marcelo', location: {lat: 42.880705, lng: -8.546967}},
-    {id: 5, name: 'Zara', location: {lat: 42.876240, lng: -8.544688}},
-    {id: 6, name: 'Follas Novas', location: {lat: 42.875189, lng: -8.549043}},
-    {id: 7, name: 'Momo Pub', location: {lat: 42.879730, lng: -8.540808}},
-    {id: 8, name: 'Café Tertulia', location: {lat: 42.880351, lng: -8.549721}},
-    {id: 9, name: 'Hostal dos reis Católicos', location: {lat: 42.881449, lng: -8.545879}}
-  ]
-
   componentDidMount() {
     VenuesAPI.getAll()
       .then(response => {
-        this.setState({locations: response.response.venues})
+        this.setState({locations: response.venues})
         console.log(this.state.locations)
       })
       .catch((error) => {
--- src/VenuesAPI.js
+++ src/VenuesAPI.js
@@ -1,5 +1,18 @@
 const api = "https://api.foursquare.com/v2/venues"
 
+export const defaultPlaces = [
+  {id: 0, name:'Praza do Obradoiro', location: {lat: 42.880593, lng: -8.545628}},
+  {id: 1, name: 'Mercado de Abastos', location: {lat: 42.879840, lng: -8.541342}},
+  {id: 2, name: 'Parque da Alameda', location: {lat: 42.876943, lng: -8.547609}},
+  {id: 3, name: 'A Maceta', location: {lat: 42.881636, lng: -8.535520}},
+  {id: 4, name: 'Casa Marcelo', location: {lat: 42.880705, lng: -8.546967}},
+  {id: 5, name: 'Zara', location: {lat: 42.876240, lng: -8.544688}},
+  {id: 6, name: 'Follas Novas', location: {lat: 42.875189, lng: -8.549043}},
+  {id: 7, name: 'Momo Pub', location: {lat: 42.879730, lng: -8.540808}},
+  {id: 8, name: 'Café Tertulia', location: {lat: 42.880351, lng: -8.549721}},
+  {id: 9, name: 'Hostal dos reis Católicos', location: {lat: 42.881449, lng: -8.545879}}
+]
+
 export const categories = [
   {"Restaurant": "4bf58dd8d48988d1c4941735"},
   {"Church": "4bf58dd8d48988d132941735"},
@@ -24,6 +37,7 @@ const apiParams = {
 export const getAll = () =>
   fetch(`${api}/search?ll=${apiParams.areaLatLng}&radius=${apiParams.radius}&intent=${apiParams.intent}&client_id=${apiParams.clientId}&client_secret=${apiParams.clientSecret}&v=${apiParams.version}&categoryId=${apiParams.categoryId}`)
    .then(response => response.json())
+   .then(data => data.response)
 
 export const getPhoto = (venue) =>
   fetch(`${api}/${venue}/photos?client_id=${apiParams.clientId}&client_secret=${apiParams.clientSecret}&v=${apiParams.version}&limit=1`)
