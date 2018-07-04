e0b68c485266ac957637cf91db0c6fee64f56132
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-07-04 11:11:36 -0700

    refactor: name of locations related variables

--- src/App.js
+++ src/App.js
@@ -1,69 +1,61 @@
 import React, { Component } from 'react';
-import * as VenuesAPI from './VenuesAPI'
-import Sidebar from './Sidebar'
-import MapContainer from './MapContainer';
+import Map from './Map';
+import Sidebar from './Sidebar';
+import * as PlacesAPI from './PlacesAPI';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      locations: [],
-      filteredLocations: []
-    }
+  state = {
+    placeList: [], 
+    placeListFiltered: [],
+    placeSelected: null
   }
 
   componentDidMount() {
     this.menu = document.getElementById('burger-icon')
     this.sidebar = document.getElementById('sidebar')
-    VenuesAPI.getAll()
-      .then(response => {
-        this.setState({locations: response.venues})
-        console.log(this.state.locations)
-      })
-      .catch((error) => {
-        console.error(error)
-      })    
+
+    PlacesAPI.getAll()
+    .then( response => {
+      this.setState({placeList: response.venues})
+      console.log(this.state.placeList)
+    })
+    .catch(error => console.error(error))
   }
 
   onMenuClick = () => {
-    this.menu.classList.toggle("change")
+    this.menu.classList.toggle("open")
     this.sidebar.classList.toggle("open")
   }
 
-  // handleSidebarFilterChange
-  sidebarFilter = (event) => {
-    let filteredVenues
+  handleSidebarFilter = (event) => {
+    let placesFiltered
     if (event === "all") {
-      filteredVenues = this.state.locations
+      placesFiltered = this.state.placeList
     } else {
-      filteredVenues = this.state.locations.filter( location => location.categories[0].id === event)
-      console.log(filteredVenues)
+      placesFiltered = this.state.placeList.filter( place => place.categories[0].id === event)
+      console.log(placesFiltered)
     }
-    this.setState({filteredLocations : filteredVenues})
-    // Close any infoWindow open
-    this.mapContainer.setState({showingInfoWindow: false})
-   }
+    this.setState({placeListFiltered : placesFiltered})
+  }
  
   render() {
-    const {filteredLocations, locations} = this.state
-    let places = (filteredLocations.length >= 1) ? filteredLocations : locations
+    const {placeList, placeListFiltered, placeSelected} = this.state
+    let places = (placeListFiltered.length >= 1) ? placeListFiltered : placeList
 
     return (
       <div className="App">
         <header className="app-header">
           <div id="burger-icon" onClick={this.onMenuClick}>
-            <div className="bar1"></div>
-            <div className="bar2"></div>
-            <div className="bar3"></div>
+            <div className="burger-icon-bar1"></div>
+            <div className="burger-icon-bar2"></div>
+            <div className="burger-icon-bar3"></div>
           </div>
           <h1 className="app-title">Explore Santiago</h1>
         </header>
         <main>
-          <Sidebar places={places} onUpdateCategory={this.sidebarFilter}/>
-          <div className="map-container">
-            <MapContainer places={places} onRef={instance => this.mapContainer = instance} /> 
-          </div>
+          <Sidebar placeList={places} onSidebarFilter={this.handleSidebarFilter}/> 
+          <Map placeList={places}/> 
         </main>
       </div>
     );
